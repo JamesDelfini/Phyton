31424687d9370df676071197cb252350e9ff02fe
Dennis James Matildo <dj.matildo@gmail.com>
2019-06-09 21:43:35 +0800

    Serialize and Deserialize into Dictionary and JSON vice versa

--- py_json.py
+++ py_json.py
@@ -1,1 +1,21 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+
+import json
+
+###################################
+## Description: Deserialize JSON
+###################################
+userJSON = '{"first_name": "Hazel", "last_name": "Nornea", "age": 21}'
+
+# Parse to Dictionary. It is like JSON.parse(<JSON_DATA>) in JavaScript
+user = json.loads(userJSON)
+print(user)
+
+print(user['first_name'])
+
+###################################
+## Description: Serialize to JSON
+###################################
+car = {'make': 'Ford', 'model': 'Mustang', 'year': 1970}
+carJSON = json.dumps(car)
+print(carJSON)
